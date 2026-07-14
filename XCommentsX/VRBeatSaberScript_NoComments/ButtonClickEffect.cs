561031749b72982d7202aabb5ceb986425574b4c
mijin <itsjinmi@gmail.com>
2026-07-14 11:33:07 +0900

    Add files via upload

---
+++ XCommentsX/VRBeatSaberScript_NoComments/ButtonClickEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ButtonClickEffect : MonoBehaviour
+{
+    [Header("--- 클릭 이펙트 설정 ---")]
+    public GameObject clickEffectPrefab;
+    public float destroyDelay = 2f;
+
+    public void PlayClickEffect()
+    {
+        if (clickEffectPrefab == null) return;
+
+        Vector3 buttonPosition = transform.position;
+
+        GameObject effect = Instantiate(clickEffectPrefab, buttonPosition, Quaternion.identity);
+
+        effect.transform.SetParent(transform.parent, false);
+        effect.transform.position = buttonPosition;
+        effect.transform.localScale = Vector3.one;
+
+        Destroy(effect, destroyDelay);
+    }
+}
+
