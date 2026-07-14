52dddbdcc5876491670cea6611893a0ecf4d8b2d
mijin <itsjinmi@gmail.com>
2026-07-14 11:29:06 +0900

    Add files via upload

---
+++ NoComments/UniRunScripts_NoComments/ScrollingObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+    public float speed = 10f;
+
+    private void Update() {
+
+        if (!GameManager.instance.isGameover)
+        { transform.Translate(Vector3.left * speed * Time.deltaTime); }
+    }
+}
